325aa58630a058a4eb8f507122aa299943095528
Pista Academy <pista.academy@gmail.com>
2023-04-23 15:45:10 +0430

    Session-4

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const http = require('http');
-
+const fs  = require('fs');
 
 
 const server = http.createServer((req, res)=>{
-    console.log('server run...')
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Message Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Pista Academy');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello to Pista Academy Node Server</h1></body>');
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(3000);
