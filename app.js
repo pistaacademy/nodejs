7d995600c2b5665ee1e1aeb8e7c8e28ec1d060e5
Pista Academy <pista.academy@gmail.com>
2023-04-23 15:21:23 +0430

    Session-2

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const http = require('http');
 
 
 const server = http.createServer((req, res)=>{
-    console.log(req);
+    console.log(req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello to Pista Academy Node Server</h1></body>');
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(3000);
