def96485821350b78b3a839b0c9f6242f06a9669
Pista Academy <pista.academy@gmail.com>
2023-04-28 18:32:00 +0430

    Session-7

--- app.js
+++ app.js
@@ -1,47 +1,7 @@
 const http = require('http');
-const fs  = require('fs');
+const routes = require('./routes')
 
 
-const server = http.createServer((req, res)=>{
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Message Page</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        // Blocking and Non-Blocking Code
-        // writeFile vs writeFileSync
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            let message = parsedBody.split('=')[1];
-            message = message.replaceAll("+", " ");
-            fs.writeFile('message.txt', message, err => {
-                console.log('level-4');
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-            console.log('level-1')
-        });
-        console.log('level-2');
-    }
-    console.log('level-3');
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello to Pista Academy Node Server</h1></body>');
-    res.write('</html>');
-    res.end();
-})
+const server = http.createServer(routes.handler)
 
 server.listen(3000);
