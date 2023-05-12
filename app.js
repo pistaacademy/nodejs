dcadda787df2edb926032490ab8a0278d339c364
Pista Academy <pista.academy@gmail.com>
2023-05-12 10:42:24 +0330

    Session-10

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const http = require('http');
+const bodyParser = require('body-parser');
 
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('I am inside first middleware!');
-    next();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></input></form>');
+});
+
+app.use('/product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
 })
 
-app.use((req, res, next)=>{
-    console.log('I am inside second middleware!');
+app.use('/',(req, res, next)=>{
     res.send('<h1>Hello from Pista Academy Express Course!</h1>');
-})
+});
 
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
