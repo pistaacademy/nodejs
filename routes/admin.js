1f8a4d55e5c7bed872636b29f022d081397e62f2
Pista Academy <pista.academy@gmail.com>
2023-05-12 17:58:17 +0330

    Session-12

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 
 router.get('/add-product',(req, res, next)=>{
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></input></form>');
+    res.sendFile(path.join(__dirname,'../', 'views', 'add-product.html'))
 });
 
 router.post('/add-product', (req, res, next)=>{
-    console.log(req.body);
-    console.log('get request')
     res.redirect('/');
 });
 
