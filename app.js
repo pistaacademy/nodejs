1e1e856f25edcebfc7583c0b95cdf1f3eaf6ec95
Pista Academy <pista.academy@gmail.com>
2023-05-12 10:22:00 +0330

    Session-9

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http');
-const routes = require('./routes')
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log('I am inside first middleware!');
+    next();
+})
+
+app.use((req, res, next)=>{
+    console.log('I am inside second middleware!');
+    res.send('<h1>Hello from Pista Academy Express Course!</h1>');
+})
 
 
-const server = http.createServer(routes.handler)
+const server = http.createServer(app);
 
 server.listen(3000);
