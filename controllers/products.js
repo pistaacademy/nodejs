13f7b13b9059d0b9ca5c76bbeba9f7410894e74c
Pista Academy <pista.academy@gmail.com>
2023-05-19 13:28:38 +0330

    Session-22

--- controllers/products.js
+++ controllers/products.js
@@ -17,13 +17,15 @@ exports.postAddProduct = (req, res, next)=>{
 }
 
 exports.getProducts = (req, res, next)=>{
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products, 
-        pageTitle: 'Pista Shop', 
-        path: '/', 
-        hasProducts: products.length > 0, 
-        activeShop: true,
-        productCSS: true
+    Product.fetchAll((products)=> {
+        res.render('shop', {
+            prods: products, 
+            pageTitle: 'Pista Shop', 
+            path: '/', 
+            hasProducts: products.length > 0, 
+            activeShop: true,
+            productCSS: true
+        });
     });
+    
 }
--- models/product.js
+++ models/product.js
@@ -1,4 +1,17 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+
+const getProductFromFile = cb => {
+        fs.readFile(p, (err, fileContent)=> {
+            if(err) {
+                cb([]);
+            } else {
+                cb(JSON.parse(fileContent));
+            }
+        })
+}
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +19,15 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getProductFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err)=> {
+                console.log(err);
+            })
+        })
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductFromFile(cb);
     }
 }
