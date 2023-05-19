d81fef63cb7bae929592934f6591484e4a41fc3f
Pista Academy <pista.academy@gmail.com>
2023-05-19 11:27:10 +0330

    Session-20

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const errorController = require('./controllers/error');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
@@ -18,11 +19,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/admin',adminData.routes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=> {
-    res.status(404).render('404', {pageTitle: '404'})
-})
+app.use(errorController.get404)
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -3,26 +3,15 @@ const path = require('path');
 
 const router = express.Router();
 
-const rootDir = require('../util/path');
-
-const products = [];
-
-router.get('/add-product',(req, res, next)=>{
-    res.render('add-product', {
-        pageTitle: 'Add Product', 
-        path: '/admin/add-product',
-        formCSS: true,
-        productCSS: true,
-        activeAddProduct : true
-    })
-});
-
-router.post('/add-product', (req, res, next)=>{
-    products.push({ title: req.body.title})
-    res.redirect('/');
-});
+const productsController = require('../controllers/products');
 
 
-exports.routes = router;
-exports.products = products;
+
+router.get('/add-product',productsController.getAddProduct);
+
+router.post('/add-product', productsController.postAddProduct);
+
+
+module.exports = router;
+
 
