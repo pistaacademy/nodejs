0eed914a95b994266bc4156bca157234d895930d
pistaacademy <pista.academy@gmail.com>
2023-05-31 06:58:35 +0330

    Session-29

--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,8 @@ router.get('/product/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart', shopController.postCart);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
