250f8f2277c79406e96ae7db0819bebf18909dae
pistaacademy <pista.academy@gmail.com>
2023-06-03 07:00:43 +0330

    Session-31

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
@@ -18,6 +19,29 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if(!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if(!product){
+      return res.redirect("/");
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
+  })
+  
 };
 
 exports.getProducts = (req, res, next) => {
