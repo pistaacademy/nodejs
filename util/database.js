78b84c6b2cd805f3d849cfe14d5c80558af79da4
Pista Academy <pista.academy@gmail.com>
2023-06-05 20:57:53 +0330

    Session-40

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    'database': 'node_complete',
-    password: 'Sepide777'
-});
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+    dialect: 'mysql',
+    host: 'localhost'
+})
 
-
-module.exports = pool.promise();
+module.exports = sequelize;
