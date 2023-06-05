9bd8fb7aee535b6a96e428d49967f0be29fda83d
Pista Academy <pista.academy@gmail.com>
2023-06-05 09:22:52 +0330

    Session-34

--- models/cart.js
+++ models/cart.js
@@ -54,4 +54,17 @@ module.exports = class Cart {
             })
         })
     }
+
+    static getCart(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if(err) {
+                cb(null)
+            } else {
+                cb(cart)
+            }
+        })
+    }
 }
+
+
