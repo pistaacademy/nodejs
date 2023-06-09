8198eba654107c0626c6bd714da5308c12013c71
Pista Academy <pista.academy@gmail.com>
2023-06-09 13:22:13 +0330

    Session-41

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
 
 const app = express();
@@ -23,4 +24,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+.then(result => {
+    app.listen(3000);
+})
+.catch(err => console.log(err))
+
+
--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node_complete', 'root', 'Sepide777', {
     dialect: 'mysql',
     host: 'localhost'
 })
