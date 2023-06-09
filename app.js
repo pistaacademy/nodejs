b917bd5911d779e44982fa79c2530b1f8271d0bd
Pista Academy <pista.academy@gmail.com>
2023-06-09 21:35:44 +0330

    Session-44

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
+const Product =  require('./models/product');
+const User = require('./models/user');
+
 
 const app = express();
 
@@ -24,7 +27,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+
+sequelize.sync({ force: true })
 .then(result => {
     app.listen(3000);
 })
