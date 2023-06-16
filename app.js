8ff1f0222dbd0da7aad0a0b9c8f438ff85fc6f37
Pista Academy <pista.academy@gmail.com>
2023-06-16 09:29:42 +0330

    Session-46

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sequelize = require('./util/database');
 
 const Product =  require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cartItem');
 
 
 const app = express();
@@ -39,8 +41,14 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
 
-sequelize.sync()
+Cart.belongsToMany(Product, { through: CartItem});
+Product.belongsToMany(Cart, { through: CartItem});
+
+
+sequelize.sync({force: true})
 .then(result => {
     return User.findByPk(1)
 })
--- controllers/admin.js
+++ controllers/admin.js
@@ -36,8 +36,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product => {
+  req.user.getProducts({ where : { id: prodId }})
+  // Product.findByPk(prodId)
+  .then(products => {
+    const product = products[0]
       if(!product){
         return res.redirect("/");
       }
@@ -78,7 +80,7 @@ exports.postEditProduct = (req,res,next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
   .then(products => {
       res.render('admin/products', {
       prods: products,
