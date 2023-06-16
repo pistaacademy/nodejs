c4f69fc0619cfa11edbab5f7b14c28f944925ce1
Pista Academy <pista.academy@gmail.com>
2023-06-16 08:12:19 +0330

    Session-45

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next)=> {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err))
+})
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -31,8 +40,18 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 
 
-sequelize.sync({ force: true })
+sequelize.sync()
 .then(result => {
+    return User.findByPk(1)
+})
+.then(user => {
+    if (!user){
+        return User.create({ name: 'Hamed', email: 'hamed@test.com'})
+    }
+    return user;
+})
+.then (user => {
+    console.log(user);
     app.listen(3000);
 })
 .catch(err => console.log(err))
