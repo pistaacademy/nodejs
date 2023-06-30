f6fa98feb30e49fcabffc15ceac2447f2a4b2f6a
Pista Academy <pista.academy@gmail.com>
2023-06-30 18:46:03 +0330

    Session-52

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-
-const Product =  require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cartItem');
-const Order = require('./models/order');
-const OrderItem = require('./models/orderItem');
-
+const mongoConnect = require('./util/database');
 
 const app = express();
 
@@ -20,58 +12,27 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next)=> {
-    User.findByPk(1)
-    .then(user => {
-        req.user = user;
-        next();
-    })
-    .catch(err => console.log(err))
+    // User.findByPk(1)
+    // .then(user => {
+    //     req.user = user;
+    //     next();
+    // })
+    // .catch(err => console.log(err))
 })
 
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-
-User.hasOne(Cart);
-Cart.belongsTo(User);
-
-Cart.belongsToMany(Product, { through: CartItem});
-Product.belongsToMany(Cart, { through: CartItem});
-
-User.hasMany(Order);
-Order.belongsTo(User);
-
-Order.belongsToMany(Product, { through: OrderItem });
-
-
-
-sequelize.sync()
-.then(result => {
-    return User.findByPk(1)
-})
-.then(user => {
-    if (!user){
-        return User.create({ name: 'Hamed', email: 'hamed@test.com'})
-    }
-    return user;
-})
-.then (user => {
-    return user.createCart();
-})
-.then (cart => {
+mongoConnect(() => {
     app.listen(3000);
 })
-.catch(err => console.log(err))
-
 
--- util/database.js
+++ util/database.js
@@ -1,8 +1,29 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node_complete', 'root', 'Sepide777', {
-    dialect: 'mysql',
-    host: 'localhost'
-})
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://pista:MqlNLNT9uZ3rNFxC@nodejs.3yhvxnz.mongodb.net/shop?retryWrites=true&w=majority'
+    )
+    
+    .then(client => {
+        _db = client.db()
+        callback();
+    })
+    .catch( err => {
+        console.log(err)
+        throw err;
+    })
+}
+
+const getDb = () => {
+    if(_db) {
+        return _db;
+    }
+    throw 'No database found!'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
