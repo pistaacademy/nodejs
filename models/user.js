8a71dacb3af3d59dca994436dc17d2b706e9039d
Pista Academy <pista.academy@gmail.com>
2023-08-25 20:15:03 +0330

    Session-60

--- models/user.js
+++ models/user.js
@@ -17,7 +17,23 @@ class User {
   }
 
   addToCart(product) {
-    const updatedCart = { items: [{productId: new ObjectId(product._id), quantity: 1}]};
+
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    })
+
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity
+    } else {
+      updatedCartItems.push({productId: new ObjectId(product._id), quantity: newQuantity})
+    }
+
+    const updatedCart = {items: updatedCartItems}
+
     const db = getDb();
     return db.collection('users').updateOne({_id: new ObjectId(this._id)},{ $set: { cart: updatedCart}})
   }
