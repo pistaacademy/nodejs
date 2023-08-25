5c1e95ae5a4450f8e8f44caf90c61e35f7ea47a2
Pista Academy <pista.academy@gmail.com>
2023-08-25 16:42:54 +0330

    Session-58

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,13 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next)=> {
-    // User.findByPk(1)
-    // .then(user => {
-    //     req.user = user;
-    //     next();
-    // })
-    // .catch(err => console.log(err))
-    next();
+    User.findById("64e8a64386959b7651db600f")
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err))
 })
 
 
