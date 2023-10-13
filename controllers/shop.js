f4326f89613af64e49183d9b0bcbea4bedeb0f92
Pista Academy <pista.academy@gmail.com>
2023-10-13 17:48:00 +0330

    Session-68

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,8 +38,11 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.user.getCart()
-  .then( products => {
+  req.user
+  .populate('cart.items.productId')
+  .then( user => {
+    console.log(user.cart.items)
+        const products = user.cart.items
         res.render('shop/cart',{
           path: '/cart',
           pageTitle: 'Your Cart',
--- routes/shop.js
+++ routes/shop.js
@@ -12,9 +12,9 @@ router.get('/products', shopController.getProducts);
 
 router.get('/product/:productId', shopController.getProduct);
 
-// router.get('/cart', shopController.getCart);
+router.get('/cart', shopController.getCart);
 
-// router.post('/cart', shopController.postCart);
+router.post('/cart', shopController.postCart);
 
 // router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
