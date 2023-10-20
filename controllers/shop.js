e61073d56d535e9067d8bd1d593fe5f4b1bec25e
Pista Academy <pista.academy@gmail.com>
2023-10-20 11:02:00 +0330

    Session-70

--- controllers/shop.js
+++ controllers/shop.js
@@ -76,7 +76,7 @@ exports.postOrder = (req, res, next) => {
     .populate('cart.items.productId')
     .then(user => {
       const products = user.cart.items.map(i => {
-        return {quantity: i.quantity, product: i.productId}
+        return {quantity: i.quantity, product: {...i.productId._doc}}
       });
       const order = new Order({
         products: products,
@@ -89,6 +89,9 @@ exports.postOrder = (req, res, next) => {
       return order.save()
     })
     .then(result => {
+      return req.user.clearCart();
+    })
+    .then(()=> {
       res.redirect('/orders');
     })
     .catch(err => console.log(err))
@@ -96,7 +99,7 @@ exports.postOrder = (req, res, next) => {
 
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders()
+  Order.find({ 'user.userId' : req.user._id})
   .then(orders => {
     res.render('shop/orders', {
       path: '/orders',
