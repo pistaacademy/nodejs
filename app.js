9b13c436bfe51212a8448f308af3f53bd2d4e3ca
Pista Academy <pista.academy@gmail.com>
2023-10-27 12:56:08 +0330

    Session-74

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require("express-session");
 
 const errorController = require('./controllers/error');
 
@@ -19,6 +20,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'pista academy', resave: false, saveUninitialized: false}))
 
 app.use((req, res, next)=> {
     User.findById("652937aa8a9c3490fd161fa6")
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 exports.getLogin = (req, res, next) => {
     // const isLoggedIn = req.get('Cookie').split(';')[2].trim().split('=')[1] === 'true'
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
@@ -8,6 +9,6 @@ exports.getLogin = (req, res, next) => {
   };
   
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
+    req.session.isLoggedIn = true;
     res.redirect('/');
   };
