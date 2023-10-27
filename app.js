7d50893cfe8be93850768de4567a4f8559ec660f
Pista Academy <pista.academy@gmail.com>
2023-10-27 19:07:12 +0330

    Session-75

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require("express-session");
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://pista:6VbWMz8BHwQTROLq@nodejs.3yhvxnz.mongodb.net/shop'
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,7 +27,12 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'pista academy', resave: false, saveUninitialized: false}))
+app.use(session({
+    secret: 'pista academy', 
+    resave: false, 
+    saveUninitialized: false, 
+    store: store
+}))
 
 app.use((req, res, next)=> {
     User.findById("652937aa8a9c3490fd161fa6")
@@ -39,7 +51,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-.connect('mongodb+srv://pista:6VbWMz8BHwQTROLq@nodejs.3yhvxnz.mongodb.net/shop?retryWrites=true&w=majority')
+.connect(MONGODB_URI)
 .then(result => {
     User.findOne().then(user => {
         if(!user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
     // const isLoggedIn = req.get('Cookie').split(';')[2].trim().split('=')[1] === 'true'
     console.log(req.session.isLoggedIn);
@@ -9,6 +11,18 @@ exports.getLogin = (req, res, next) => {
   };
   
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('652937aa8a9c3490fd161fa6')
+    .then (user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+  };
+
+  exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+      console.log(err);
+      res.redirect('/');
+    })
   };
