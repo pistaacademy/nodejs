a0a393124c76f6d33ec79b4843a480ef1498bf08
Pista Academy <pista.academy@gmail.com>
2023-11-17 19:33:20 +0330

    Session-81

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const isAuth = require('../middleware/is-auth');
 
 const express = require('express');
 
@@ -7,18 +8,18 @@ const adminController = require('../controllers/admin');
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+router.get('/add-product',isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products', adminController.getProducts);
+router.get('/products',isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product',isAuth, adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId',isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', adminController.postEditProduct);
+router.post('/edit-product',isAuth, adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/delete-product',isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
