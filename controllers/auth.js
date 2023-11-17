8d121cf0f8356782e2a932baf147ad4920a58669
Pista Academy <pista.academy@gmail.com>
2023-11-17 17:34:04 +0330

    Session-79

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs')
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -39,16 +40,19 @@ exports.postSignup = (req, res, next) => {
       if(userDoc) {
         return res.redirect('/signup')
       }
-      const user = new User({
-        email: email,
-        password: password,
-        cart: {items: []}
-      });
-      return user.save();
-    })
-    .then(result => {
-      res.redirect('/login');
-    })
+      return bcrypt.hash(password, 12)
+      .then(hashedPassword => {
+        const user = new User({
+          email: email,
+          password: hashedPassword,
+          cart: {items: []}
+        });
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/login');
+      })
+      })
     .catch(err => {
       console.log(err)
     })
