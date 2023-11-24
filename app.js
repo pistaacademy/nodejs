8b6ab0a1f4dd95612716887f70b1acab2313c86a
Pista Academy <pista.academy@gmail.com>
2023-11-24 19:22:53 +0330

    Session-84

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const csrf = require('csurf')
+const csrf = require('csurf');
+const flash = require('connect-flash')
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -39,6 +40,7 @@ app.use(
 );
 
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    errorMessage: req.flash('error')
   });
 };
 
@@ -13,7 +13,6 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
   });
 };
 
@@ -24,6 +23,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({email: email})
   .then(user => {
     if(!user) {
+      req.flash('error', 'Invalid email or password')
       return res.redirect('/login');
     }
     bcrypt.compare(password, user.password)
