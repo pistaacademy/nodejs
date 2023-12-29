d4db9dc8391b2fed9ffbceeeaee31f9fa7d47fe7
Pista Academy <pista.academy@gmail.com>
2023-12-29 21:32:31 +0330

    Session-92

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator')
+const { check, body } = require('express-validator')
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +11,23 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup',check('email').isEmail().withMessage('Please enter a valid email!'), authController.postSignup);
+router.post('/signup',
+    [
+        check('email').isEmail().withMessage('Please enter a valid email!')
+        .custom((value, { req }) => {
+            if(value === 'iran@iran.com') {
+                throw new Error('This email address is forbidden!!!')
+            }
+            return true;
+        }),
+        body(
+            'password', 
+            'Please enter a password with only numbers abd text and at least 5 characters')
+            .isLength({ min: 5 })
+            .isAlphanumeric()
+    ],
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
