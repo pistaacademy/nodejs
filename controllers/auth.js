76beaf670f2f48bd6ef99591ac0ace73dbaa001f
Pista Academy <pista.academy@gmail.com>
2024-01-12 10:12:44 +0330

    Session-94

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,12 +29,27 @@ exports.getSignup = (req, res, next) => {
     path: '/signup',
     pageTitle: 'Signup',
     errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    },
+    validationErrors: [],
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg
+    });
+  }
 
   User.findOne({email: email})
   .then(user => {
@@ -70,10 +85,13 @@ exports.postSignup = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
+    console.log(errors)
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'signup',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: { email: email, password: password, confirmPassword: req.body.confirmPassword},
+      validationErrors: errors.array(),
     })
   }
   bcrypt.hash(password, 12)
