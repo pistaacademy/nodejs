681be11b9b6886dfb72a77de8eb6b1b86ff99533
Pista Academy <pista.academy@gmail.com>
2024-01-12 18:23:05 +0330

    Session-96

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,17 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login', [
+    body('email')
+    .isEmail()
+    .withMessage('Please enter a valid email address.')
+    .normalizeEmail(),
+    body('password', 'Password has to be valid')
+    .isLength({ min: 5 })
+    .isAlphanumeric()
+    .trim()
+],
+authController.postLogin);
 
 router.post('/signup',
     [
@@ -22,13 +32,14 @@ router.post('/signup',
                         return Promise.reject('E-Mail exists already, please pick a different one.')
                     }
             });
-        }),
+        }).normalizeEmail(),
         body(
             'password', 
             'Please enter a password with only numbers abd text and at least 5 characters')
             .isLength({ min: 5 })
+            .trim()
             .isAlphanumeric(),
-            body('confirmPassword').custom((value, { req })=> {
+            body('confirmPassword').trim().custom((value, { req })=> {
                 if (value !== req.body.password) {
                     throw new Error('Passwords have to match!')
                 }
