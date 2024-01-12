76f5e4af544e9f22ff3706fc6a3b5793f3420484
Pista Academy <pista.academy@gmail.com>
2024-01-12 20:24:59 +0330

    Session-97

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,13 @@
 const Product = require('../models/product');
+const { validationResult } = require('express-validator')
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
+    hasError: false,
+    errorMessage: null,
   });
 };
 
@@ -13,6 +16,24 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description
+      },
+      errorMessage: errors.array()[0].msg,
+    })
+  }
+
   const product = new Product({
     title: title,
     price: price,
@@ -48,6 +69,8 @@ exports.getEditProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
+        hasError: true,
+        errorMessage: null,
       });
     })
     .catch(err => console.log(err));
