8dde163e04853f8d16704569f56b7bc75dde7ab7
Pista Academy <pista.academy@gmail.com>
2024-02-11 07:49:03 +0330

    Session-102

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
@@ -55,16 +61,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => {
-      throw new Error(err);
+      next(new Error(err))
     }
     );
 });
 
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -74,7 +76,11 @@ app.use('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next)=> {
-  res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Server Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
 });
 
 mongoose
